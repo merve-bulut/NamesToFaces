db44eea33d163f28a001b9ab33c6e9588f9e4acf
merve-bulut <mervebult6@gmail.com>
2026-09-05 12:45:41 +0300

    Add Person Cell

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Merve Bulut on 5.09.2026.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
