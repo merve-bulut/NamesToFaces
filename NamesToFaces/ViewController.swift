5bdf346fa2b2f948cb79c5a7c2d357fde99fc3fa
merve-bulut <mervebult6@gmail.com>
2026-09-05 18:10:14 +0300

    Save picked image to documents directory

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -32,5 +32,23 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         picker.delegate = self
         present(picker, animated: true)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+            
+            dismiss(animated: true)
+        }
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
